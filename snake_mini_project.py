4c1eaba86ef43a028388db01830c055a3f01d20e
roni20-meet <roni20@meet.mit.edu>
2018-08-09 12:30:18 +0300

    Stopping here

--- snake_mini_project.py
+++ snake_mini_project.py
@@ -9,10 +9,27 @@ Date:
 import turtle
 import random #We'll need this later in the lab
 
-turtle.tracer(1,0) #This helps the turtle move more smoothly
+moshiko = turtle.Turtle()
+moshiko.pu()
+moshiko.ht()
+moshiko.goto(-200,300)
+moshiko.write("SNAKE GAME!!!", font=("Arial" , 40, "normal"))
 
-SIZE_X=800
-SIZE_Y=500
+turtle.tracer(1,0) #This helps the turtle move more smoothly
+border = turtle.clone()
+border.showturtle()
+border.shape("turtle")
+border.penup()
+border.goto(-300,300)
+border.pendown()
+border.goto(300,300)
+border.goto(300,-300)
+border.goto(-300,-300)
+border.goto(-300,300)
+border.hideturtle()
+counter =0
+SIZE_X=1000
+SIZE_Y=1000
 turtle.setup(SIZE_X, SIZE_Y) #Curious? It's the turtle window  
                              #size. 
 turtle.penup()
@@ -36,24 +53,24 @@ turtle.hideturtle()
 #Draw a snake at the start of the game with a for loop
 #for loop should use range() and count up to the number of pieces
 #in the snake (i.e. START_LENGTH)
-#for ______  in __________ :
-#    x_pos=snake._____________ #Get x-position with snake.pos()[0]
-#    y_pos=snake._____________ 
-#
-#    #Add SQUARE_SIZE to x_pos. Where does x_pos point to now?    
-#    # You're RIGHT!
-#    x_pos+=__________ 
-#
-#    my_pos=(x_pos,y_pos) #Store position variables in a tuple
-#    snake.goto(____________,____________) #Move snake to new (x,y)
-#   
-#    #Append the new position tuple to pos_list
-#    ______.append(_____________) 
-#
-#    #Save the stamp ID! You'll need to erase it later. Then append
-#    # it to stamp_list.             
-#    _____________ = snake.stamp()
-#    ______.append(_____________)
+for i  in range(START_LENGTH):
+    x_pos=snake.pos()[0] #Get x-position with snake.pos()[0]
+    y_pos=snake.pos()[1] 
+
+    #Add SQUARE_SIZE to x_pos. Where does x_pos point to now?    
+    # You're RIGHT!
+    x_pos+=20
+
+    my_pos=(x_pos,y_pos) #Store position variables in a tuple
+    snake.goto(x_pos,y_pos) #Move snake to new (x,y)
+   
+    #Append the new position tuple to pos_list
+    pos_list.append(my_pos) 
+
+    #Save the stamp ID! You'll need to erase it later. Then append
+    # it to stamp_list.             
+    snake_stamp = snake.stamp()
+    stamp_list.append(snake_stamp)
 
 
 ###############################################################
@@ -69,88 +86,167 @@ TIME_STEP = 100 #Update snake position after this many
 SPACEBAR = "space" # Careful, it's not supposed to be capitalized!
 
 UP = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
 #1. Make variables LEFT, DOWN, and RIGHT with values 1, 2, and 3
 ####WRITE YOUR CODE HERE!!
-#
-#direction = UP
-#
-#def up():
-#    global direction #snake direction is global (same everywhere)
-#    direction=UP #Change direction to up
-#    move_snake() #Update the snake drawing <- remember me later
-#    print("You pressed the up key!")
-#
-##2. Make functions down(), left(), and right() that change direction
-#####WRITE YOUR CODE HERE!!
-#
-#turtle.onkeypress(up, UP_ARROW) # Create listener for up key
-#
-##3. Do the same for the other arrow keys
-#####WRITE YOUR CODE HERE!!
-#
-#turtle.listen()
-#
-#def move_snake():
-#    my_pos = snake.pos()
-#    x_pos = my_pos[0]
-#    y_pos = my_pos[1]
-#    
-#    if direction==RIGHT:
-#        snake.goto(x_pos + SQUARE_SIZE, y_pos)
-#        print(“You moved right!”)
-#    elif direction==LEFT:
-#        snake.goto(x_pos - SQUARE_SIZE, y_pos)
-#        print(“You moved left!”)
-#
-#    #4. Write the conditions for UP and DOWN on your own
-#    ##### YOUR CODE HERE
-#
-#    #Stamp new element and append new stamp in list
-#    #Remember: The snake position changed - update my_pos()
-#
-#    my_pos=snake.pos() 
-#    pos_list.append(my_pos)
-#    new_stamp = snake.stamp()
-#    stamp_list.append(new_stamp)
-#    ######## SPECIAL PLACE - Remember it for Part 5
-#    #pop zeroth element in pos_list to get rid of last the last 
-#    #piece of the tail
-#    old_stamp = stamp_list.pop(0)
-#    snake.clearstamp(old_stamp)
-#    pos_list.pop(0)
+
+direction = UP
+
+UP_EDGE = 300
+DOWN_EDGE = -300
+RIGHT_EDGE = 300
+LEFT_EDGE = -300
+
+def up():
+    global direction #snake direction is global (same everywhere)
+    direction=UP #Change direction to up
+     #Update the snake drawing <- remember me later
+    print("You pressed the up key!")
 
 
-###################################################################
-#           PART 3
-####################################################################
-#
-##Go to the top of your file, and after the line that says direction = UP,  write:
-#
-#UP_EDGE = 250
-#DOWN_EDGE = -250
-#RIGHT_EDGE = 400
-#LEFT_EDGE = -400
-#
-#
-##Now go add code to the end of your  move_snake() function
-#
-#def move_snake():
-#
-#    . . .
-#
-#    #Add new lines to the end of the function
-#    #Grab position of snake
-#    new_pos = snake.pos()
-#    new_x_pos = new_pos[0]
-#    new_y_pos = new_pos[1]
-#
-## The next three lines check if the snake is hitting the 
-## right edge.
-#if new_x_pos >= RIGHT_EDGE:
-#print(“You hit the right edge! Game over!”)
-#quit()
-#
-#     # You should write code to check for the left, top, and bottom edges.
-#    #####WRITE YOUR CODE HERE
+def down():
+    global direction #snake direction is global (same everywhere)
+    direction=DOWN #Change direction to down
+     #Update the snake drawing <- remember me later
+    print("You pressed the down key!")
 
 
+def left():
+    global direction #snake direction is global (same everywhere)
+    direction=LEFT #Change direction to up
+     #Update the snake drawing <- remember me later
+    print("You pressed the left key!")
+
+
+def right():
+    global direction #snake direction is global (same everywhere)
+    direction=RIGHT #Change direction to up
+     #Update the snake drawing <- remember me later
+    print("You pressed the right key!")
+
+#2. Make functions down(), left(), and right() that change direction
+####WRITE YOUR CODE HERE!!
+
+turtle.onkeypress(up, UP_ARROW) # Create listener for up key
+turtle.onkeypress(down, DOWN_ARROW)
+turtle.onkeypress(right,RIGHT_ARROW)
+turtle.onkeypress(left,LEFT_ARROW)
+#3. Do the same for the other arrow keys
+####WRITE YOUR CODE HERE!!
+
+turtle.listen()
+
+def make_food():
+    #The screen positions go from -SIZE/2 to +SIZE/2
+    #But we need to make food pieces only appear on game squares
+    #So we cut up the game board into multiples of SQUARE_SIZE.
+    min_x=-int(300/2/SQUARE_SIZE)+1
+    max_x=int(300/2/SQUARE_SIZE)-1
+    min_y=-int(300/2/SQUARE_SIZE)-1
+    max_y=int(300/2/SQUARE_SIZE)+1
+    
+    #Pick a position that is a random multiple of SQUARE_SIZE
+    food_x = random.randint(min_x,max_x)*SQUARE_SIZE
+    food_y = random.randint(min_y,max_y)*SQUARE_SIZE
+
+    food.goto(food_x,food_y)
+    food_pos.append(food.pos())
+    foodpos = food.stamp()
+    food_stamps.append(foodpos)
+def move_snake():
+    my_pos = snake.pos()
+    x_pos = my_pos[0]
+    y_pos = my_pos[1]
+    
+    if direction==RIGHT:
+        snake.goto(x_pos + SQUARE_SIZE, y_pos)
+        print("You moved right!")
+    elif direction==LEFT:
+        snake.goto(x_pos - SQUARE_SIZE, y_pos)
+        print("You moved left!")
+    elif direction==DOWN:
+        snake.goto(x_pos,y_pos - SQUARE_SIZE)
+        print('You moved down!')
+    elif direction==UP:
+        snake.goto(x_pos,y_pos + SQUARE_SIZE)
+        print('you moved up!')
+    #4. Write the conditions for UP and DOWN on your own
+    ##### YOUR CODE HERE
+
+    #Stamp new element and append new stamp in list
+    #Remember: The snake position changed - update my_pos()
+
+    my_pos=snake.pos() 
+    pos_list.append(my_pos)
+    new_stamp = snake.stamp()
+    stamp_list.append(new_stamp)
+    ######## SPECIAL PLACE - Remember it for Part 5
+    #pop zeroth element in pos_list to get rid of last the last 
+    #piece of the tail
+    global food_stamps, food_pos
+    #If snake is on top of food item
+    if snake.pos() in food_pos:
+        food_ind=food_pos.index(snake.pos()) #What does this do?
+        food.clearstamp(food_stamps[food_ind]) #Remove eaten food                 
+                                              #stamp
+        food_pos.pop(food_ind) #Remove eaten food position
+        food_stamps.pop(food_ind) #Remove eaten food stamp
+        print('You have eaten the food!')
+        global counter
+        counter+=1
+        turtle.undo()
+        turtle.hideturtle()
+        turtle.goto(0,-400)
+        turtle.write(counter,font=("Arial",30,"normal"))
+    
+#HINT: This if statement may be useful for Part 8
+    else:
+        old_stamp = stamp_list.pop(0)
+        snake.clearstamp(old_stamp)
+        pos_list.pop(0)
+    
+    new_pos = snake.pos()
+    new_x_pos = new_pos[0]
+    new_y_pos = new_pos[1]
+
+    if new_x_pos >= RIGHT_EDGE:
+        print("You hit the right edge! Game over!")
+        quit()
+    elif new_x_pos <= LEFT_EDGE:
+        print("You hit the left edge! Game over!")
+        quit()
+    elif new_y_pos <= DOWN_EDGE:
+        print("You hit the down edge! Game over!")
+        quit()
+    elif new_y_pos >= UP_EDGE:
+        print("You hit the up edge! Game over!")
+        quit()
+    turtle.ontimer(move_snake,TIME_STEP)
+
+    if len(food_stamps) <= 6 :
+        make_food()
+
+    if pos_list[-1] in pos_list[:-1]:
+        print('you hit a part of your body')
+        quit()
+
+
+
+turtle.register_shape("trash.gif")
+food = turtle.clone()
+food.shape("trash.gif") 
+food_pos = [(100,100), (-100,100), (-100,-100), (100,-100)]
+food_stamps = []
+food.hideturtle()
+food.penup()
+
+for this_food_pos in food_pos:
+    food.goto(this_food_pos)
+    foodStamp = food.stamp()
+    food_stamps.append(foodStamp)
+
+
+    
+move_snake()
